7821f63aaae3b99b4a4f3f24a22b24d1614be5fa
Tinsou-Christ <anosvoldigo291@gmail.com>
2026-07-08 10:19:32 +0000

    Update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,7 +4,6 @@ const MONGO_URI = process.env.MONGO_URI as string;
 
 export const connectDB = async () => {
   try {
-    console.log(process.env.MONGO_URI);
     await mongoose.connect(MONGO_URI);
     console.log("✅ Connected to MongoDB");
   } catch (error) {
